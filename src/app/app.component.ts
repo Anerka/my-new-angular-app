a7424862112390d6d579ba121b13f929bc13581b
Anders <enkardell@gmail.com>
2021-04-26 09:08:10 +0200

    input/output from and to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,50 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+
+export class AppComponent {
+  newUserName: string = ''
+  users = [
+    {
+        id: "1",
+        name: 'Jack',
+        age: 21
+    },
+    {
+        id: "2",
+        name: 'James',
+        age: 22
+    },
+    {
+        id: "3",
+        name: 'Johan',
+        age: 23
+    }
+  ]
+
+  removeUser(id: string): void {
+    this.users = this.users.filter(user => user.id !== id)
+  console.log('removeUser', id) 
+  }
+
+  addUser(name: string) : void {
+    if(this.newUserName != '')
+    {
+        var uniqueId = 0
+        if(this.users.length > 0)
+            uniqueId = Math.max.apply(Math, this.users.map(function(o) { return +o.id; })) + 1
+            //uniqueId = 10
+        else
+            uniqueId = 1
+
+    const newUser = {
+        id: uniqueId.toString(),
+        name,
+        age: 30
+    }
+    this.users.push(newUser)
+    console.log('addUser ')
+    }
+}
+
+}
--- src/app/usersList/usersList.component.ts
+++ src/app/usersList/usersList.component.ts
@@ -1,39 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector:'app-users-list',
     templateUrl: './usersList.component.html',
+    styleUrls: ['./usersList.component.scss']
 })
 
 export class UsersListComponent {
-    users = [
-        {
-            id: '1',
-            name: 'Jack',
-            age: 21
-        },
-        {
-            id: '2',
-            name: 'James',
-            age: 22
-        },
-        {
-            id: '3',
-            name: 'Johan',
-            age: 23
-        }
-    ]
+    @Input() users
+    @Output() removeUser = new EventEmitter()
+    newUserName: string = ''
 
-    removeUser(id: string): void {
-        this.users = this.users.filter(user => user.id !== id)
-       console.log('removeUser', id) 
-    }
-
-    setNewUserName(userName: string) : void {
-        console.log('setNewUserName', userName )
+    setNewUserName(event: Event) : void {
+        this.newUserName = (<HTMLInputElement>event.target).value
+        console.log('setNewUserName', this.newUserName )
     }
 
     addUser() : void {
+        if(this.newUserName != '')
+        {
+            var uniqueId = 0
+            if(this.users.length > 0)
+                uniqueId = Math.max.apply(Math, this.users.map(function(o) { return +o.id; })) + 1
+                //uniqueId = 10
+            else
+                uniqueId = 1
+
+        const newUser = {
+            id: uniqueId.toString(),
+            name: this.newUserName,
+            age: 30
+        }
+        this.users.push(newUser)
+        this.newUserName = ''
         console.log('addUser ')
+        }
     }
 }
